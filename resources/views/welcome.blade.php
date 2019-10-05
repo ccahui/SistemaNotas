039549346c0445c7bdb70b9e0a2aa664193bf51b
ccahui <kris_ti4n@hotmail.com>
2019-10-05 09:37:29 -0500

    Vista de Iniciar Sesion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,18 @@
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title> Sistema Notas</title>
+    
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+          <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+          <!-- Compiled and minified JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+       <!--Import Google Icon Font-->
+       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -19,37 +26,20 @@
                 background-color: #fff;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
+                font-weight: 300;
             }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
+          
             .content {
                 text-align: center;
             }
 
             .title {
-                font-size: 84px;
+                font-size: 74px;
+                margin-top: 20px;
+            }
+            .subtitle{
+                font-size: 20px;
+                font-weight: 500;
             }
 
             .links > a {
@@ -61,23 +51,53 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
-            .m-b-md {
-                margin-bottom: 30px;
+            .mt{
+                margin-top: 100px;
             }
+            .mx{
+    margin-left: 4rem !important;
+    margin-right: 4rem !important;
+}
 
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div>
             
             <div class="content">
-                <div class="title m-b-md">
-                    Sistema Notas
+                <div class="title ">
+                    Iniciar Sesión
                 </div>
             </div>
+            
         </div>
-        
+
+            <div class="content subtitle">
+                Sistema de notas
+            </div>
+
+        <div class="content mt">
+                <a href="" class="">
+                        <div style="display: inline-block">
+                                <i class="material-icons large mx">account_circle</i>
+                                <span style="display: block">Alumno</span>
+                        </div>
+                    
+                </a>
+                <a href="" >
+                        <div style="display: inline-block">
+                                <i class="material-icons large mx">people</i>
+                                <span style="display: block">Profesor</span>
+                        </div>
+                </a>
+                <a href="">
+                        <div style="display: inline-block">
+                                <i class="material-icons large mx">supervised_user_circle</i>
+                                <span style="display: block">Admin</span>
+                        </div>    
+                    </a>
+
+        </div>
         
     </body>
 </html>
