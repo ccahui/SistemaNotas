e84b683160e7ed306b00cb15e7db22c20ba290fa
ccahui <kccahui@unsa.edu.pe>
2019-11-07 21:20:22 -0500

    Stylos Tabs

--- resources/views/profesoresAsignar/detalle.blade.php
+++ resources/views/profesoresAsignar/detalle.blade.php
@@ -18,54 +18,54 @@
 <div>
 
   <form action="{{url("/profesores/asignarCurso")}}" method="post">
-      {{ csrf_field() }}
-      <input name="profesor_id" hidden type="text" value="{{$profesor->id}}">
+    {{ csrf_field() }}
+    <input name="profesor_id" hidden type="text" value="{{$profesor->id}}">
 
-      <div class="row">
-    @foreach ($grados as $grado)
-    <div id="{{$grado->id}}" class="col ">
-      <table>
-        <thead>
+    <div class="row">
+      @foreach ($grados as $grado)
+      <div id="{{$grado->id}}" class="col ">
+        <table>
+          <thead>
+            <tr>
+              <th>Curso</th>
+              <th>Secciones</th>
+            </tr>
+          </thead>
+          @foreach ($grado->cursos as $curso)
+
           <tr>
-            <th>Curso</th>
-            <th>Secciones</th>
+            <td> {{$curso->nombre}} </td>
+            <td>
+              @foreach ($curso->salones as $salon)
+
+
+              <label class="mx-2">
+                <input type="checkbox" name="malla_ids[]" value="{{$salon->pivot->id}}" class="filled-in" />
+                <span>{{$salon->seccion}}</span>
+              </label>
+
+
+
+              @endforeach
+
+            </td>
           </tr>
-        </thead>
-        @foreach ($grado->cursos as $curso)
-
-        <tr>
-          <td> {{$curso->nombre}} </td>
-          <td>
-            @foreach ($curso->salones as $salon)
-
-
-            <label class="mx-2">
-            <input type="checkbox" name="malla_ids[]"  value="{{$salon->pivot->id}}" class="filled-in" />
-              <span>{{$salon->seccion}}</span>
-            </label>
-
-
-
-            @endforeach
-
-          </td>
-        </tr>
-        @endforeach
-        <tbody>
-        </tbody>
-      </table>
+          @endforeach
+          <tbody>
+          </tbody>
+        </table>
 
 
 
 
 
+      </div>
+      @endforeach
     </div>
-    @endforeach
-  </div>
-  <div class="">
-    <button type="submit" class="btn blue">Guardar Cambios</button>
-  <a href="{{url("/profesores/asignarCurso")}}" class="btn ">Cancelar</a>
-  </div>
+    <div class="">
+      <button type="submit" class="btn blue">Guardar Cambios</button>
+      <a href="{{url("/profesores/asignarCurso")}}" class="btn ">Cancelar</a>
+    </div>
   </form>
 </div>
 
